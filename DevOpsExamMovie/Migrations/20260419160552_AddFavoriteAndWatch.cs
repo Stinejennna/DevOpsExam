147fe130a58b1e10b4cd687968a46f7a770c977a
Stinejennna <stinejenna99@gmail.com>
2026-04-19 19:18:35 +0200

    .

---
+++ DevOpsExamMovie/Migrations/20260419160552_AddFavoriteAndWatch.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevOpsExamMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavoriteAndWatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Movies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ToWatch",
+                table: "Movies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ToWatch",
+                table: "Movies");
+        }
+    }
+}
