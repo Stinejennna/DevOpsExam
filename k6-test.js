a35cfca1749b313fb762a65675816c63db6a09e5
Stinejennna <stinejenna99@gmail.com>
2026-06-13 19:29:57 +0200

    Security changes

--- k6-test.js
+++ k6-test.js
@@ -2,7 +2,7 @@
 import { check } from 'k6';
 
 export default function () {
-    let res = http.get('http://185.196.21.234:8000/api/movies');
+    let res = http.get('https://185.196.21.234:8000/api/movies');
     check(res, {
         'status is 200': (r) => r.status === 200
     });
