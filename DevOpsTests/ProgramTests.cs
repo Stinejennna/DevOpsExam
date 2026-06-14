32ab9674fb6b0e3db1f16e22282fde4dcc13e841
Stinejennna <stinejenna99@gmail.com>
2026-06-14 15:10:30 +0200

    Added more tests

--- DevOpsTests/ProgramTests.cs
+++ DevOpsTests/ProgramTests.cs
@@ -20,12 +20,50 @@ public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
             builder.UseEnvironment("Testing");
         });
     }
+    
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Production"); 
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+                services.RemoveAll(typeof(AppDbContext));
+                
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseInMemoryDatabase("MigrationTestDb"));
+            });
+        }
+    }
 
     [Fact]
     public void Application_StartsAndRegistersServices_Successfully()
     {
         var client = _factory.CreateClient();
         
+        Assert.NotNull(client);
+    }
+    
+    [Fact]
+    public async Task AppStartup_WhenNotTesting_ExecutesMigration()
+    {
+        using var factory = new CustomWebApplicationFactory();
+        
+        var client = factory.CreateClient();
+        
+        Assert.NotNull(client);
+    }
+    
+    [Fact]
+    public void Application_Starts_InDevelopmentMode_MapsOpenApi()
+    {
+        var factory = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.UseEnvironment("Development");
+        });
+
+        var client = factory.CreateClient();
         Assert.NotNull(client);
     }
 }
