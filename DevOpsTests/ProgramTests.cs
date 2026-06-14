bdad49bb6fc573842de2967437c2c9239de4d036
Stinejennna <stinejenna99@gmail.com>
2026-06-14 15:21:08 +0200

    Fixed program tests

--- DevOpsTests/ProgramTests.cs
+++ DevOpsTests/ProgramTests.cs
@@ -9,24 +9,16 @@ using Xunit;
 
 namespace DevOpsTests;
 
-public class ProgramTests
+public class ProgramTests 
 {
-    private class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
         private readonly string _env;
-        public CustomWebApplicationFactory(string env = "Production") => _env = env;
+        public CustomWebApplicationFactory(string env) => _env = env;
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment(_env); 
-            builder.ConfigureServices(services =>
-            {
-                services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
-                services.RemoveAll(typeof(AppDbContext));
-                
-                services.AddDbContext<AppDbContext>(options =>
-                    options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
-            });
         }
     }
 
@@ -41,7 +33,7 @@ public class ProgramTests
     [Fact]
     public async Task AppStartup_WhenNotTesting_ExecutesMigration()
     {
-        using var factory = new CustomWebApplicationFactory("Production");
+        using var factory = new CustomWebApplicationFactory("Production_Test");
         var client = factory.CreateClient();
         Assert.NotNull(client);
     }
